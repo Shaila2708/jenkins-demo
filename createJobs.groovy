659bc30b7e66c29fe694fcdcb26773020b3d7b21
Seshu <seshadri.allimani@in.ibm.com>
2021-07-11 12:07:16 +0530

    my change

--- createJobs.groovy
+++ createJobs.groovy
@@ -1,3 +1,25 @@
+pipelineJob('pipelineJob') {
+    definition {
+        cps {
+            script(readFileFromWorkspace('pipelineJob.groovy'))
+            sandbox()
+        }
+    }
+}
+pipelineJob('theme-park-job') {
+    definition {
+        cpsScm {
+            scm {
+                git {
+                    remote {
+                        url 'https://github.com/seshadriallimani/spring-boot-api-example.git'
+                    }
+                    branch 'master'
+                }
+            }
+        }
+    }
+}
 pipelineJob('theme-park-job-docker') {
     definition {
         cpsScm {
